f6bc1301069682c97b40fd8ffd667a833491c85d
ebarrier <ebarrier@itcollege.ee>
2016-03-17 15:19:58 +0200

    Geoip lookup and report generation

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ from jinja2 import Environment, FileSystemLoader #This is the templating engine 
 import os
 import urllib
 
+# This will contain main.py and templates
+PROJECT_ROOT = os.path.dirname(__file__)
+
 # Following is the directory with log files,
 # On Windows substitute it where you downloaded the files
 parser = argparse.ArgumentParser(description='Apache2 log parser.')
 
-parser.add_argument('--path', default="/home/ebarrier/logs", help='Path to Apache2 log files')
+parser.add_argument('--path', default="/home/ebarrier/Documents/Python", help='Path to Apache2 log files')
 parser.add_argument('--topurls', help="Find top URL-s", action='store_true')
 parser.add_argument('--geoip', help ="Path to file to resolve IPs to country codes", action='store_true', default='/usr/share/GeoIP/GeoIP.dat')
 parser.add_argument('--verbose', help="Let's chat", action="store_true")
-
+parser.add_argument('--output', help="This is where we place the output files such as report.html and map.svg", default='build')
 args = parser.parse_args()
 
-gi = GeoIP.open(args.geoip, GeoIP.GEOIP_MEMORY_CACHE) #download list of countries with their IPs
-print "We are expecting logs from:", args.path
-print "Do we want top URL-s?", args.topurls
+try:
+    gi = GeoIP.open(args.geoip, GeoIP.GEOIP_MEMORY_CACHE) #download list of countries with their IPs
+except:
+    print "Failed to open up GeoIP database, are you sure %s exists?" % args.geoip
+    exit(255)
 
 keywords = "Windows", "Linux", "OS X", "Ubuntu", "Googlebot", "bingbot", "Android", "YandexBot", "facebookexternalhit"
 d = {} # Curly braces define empty dictionary
 urls={} #dictionary for urls
-users={} #dictionary for users
+user_bytes={} #dictionary for users
 ip_addresses={} #Ips
 countries={} #list of countries
 
@@ -67,9 +72,9 @@ for filename in os.listdir(args.path):
                                                         #"user" will be assign to the first part of the split. 
                                                         #"remainder" will be assigned the second part of the split.
                 try:
-                    users[user] = users[user] + 1
+                    user_bytes[user] = user_bytes[user] + 1
                 except:
-                    users[user] = 1
+                    user_bytes[user] = 1
 
             #to count the urls
             url = "httl://enos.itcollege.ee" + path
@@ -89,10 +94,20 @@ for filename in os.listdir(args.path):
         except ValueError:
             pass # This will do nothing, needed due to syntax
 
+def humanize(bytes):
+    if bytes<1024:
+        return "%d B" % bytes
+    elif bytes<1024**2:
+        return "%.1f kB" % (bytes / 1024.0)
+    elif bytes < 1024**3:
+        return "%.1f MB" % (bytes / 1024.0**2)
+    else:
+        return "%.1f GB" % (bytes / 1024.0**3)
+
 from lxml import etree
 from lxml.cssselect import CSSSelector
 
-document =  etree.parse(open('BlankMap-World6.svg'))
+document =  etree.parse(open(os.path.join(PROJECT_ROOT, 'templates', 'map.svg')))
 
 max_hits = max(countries.values())
 print("country with max amount of hits:", max_hits)
@@ -104,36 +119,40 @@ for country_code, hits in countries.items():
     sel = CSSSelector("#" + country_code.lower())
     for j in sel(document):
         #line below can be another way written j.set("style", "fill:#" + hex(hits * 255 / max_hits)[2:] + "0000")
-        red = hits * 255 / max_hits
+        ##red = hits * 255 / max_hits
         #j.set("style", "fill:#%02x%02x00" % (red, 255-red)) # from green to red through yellow
         #Instead of RGB, it is better to use hue saturation luma color coding (from 0 to 360).
-        j.set("style", "fill:hsl(%d, 90%%, 70%%);" % (120 + (0 * 120 / max_hits)))
+        j.set("style", "fill:hsl(%d, 90%%, 70%%);" % (120 - hits * 120 / max_hits))
 
         # Remove styling from children
         for i in j.iterfind("{http://www.w3.org/2000/svg}path"):
             i.attrib.pop("class", "")
 
-with open("highlighted.svg", "w") as fh:
+with open(os.path.join(args.output, "map.svg"), "w") as fh:
     fh.write(etree.tostring(document))
 
-def humanize(bytes):
-    if bytes<1024:
-        return "%d B" % bytes
-    elif bytes<1024**2:
-        return "%.1f kB" % (bytes / 1024.0)
-    elif bytes < 1024**3:
-        return "%.1f MB" % (bytes / 1024.0**2)
-    else:
-        return "%.1f GB" % (bytes / 1024.0**3)
+from jinja2 import Environment, FileSystemLoader # This it the templating engine we will use
 
-users = sorted(users.items(), key = lambda item:item[1], reverse=True)
-urls = sorted(urls.items(), key = lambda item:item[1], reverse=True)
-env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)), trim_blocks=True)
-with codecs.open("output.html", "w", encoding="utf-8") as fh:
-    fh.write(env.get_template("report.html").render(locals())) #locals() is a dict which contains all above defined variables.
-                                                               #if variable is defined after after in the code, it is not taken into acccount
-#os.system("x-www-browser file://" + os.path.realpath("output.html") + " &")
-    
+env = Environment(loader=FileSystemLoader(os.path.join(PROJECT_ROOT, "templates")), trim_blocks=True)
+
+import codecs
+
+# This is the context variable for our template, these are the only
+# variables that can be accessed inside template
+
+context = {
+    "humanize": humanize, # This is why we use locals() :D
+    "url_hits": sorted(urls.items(), key=lambda i:i[1], reverse=True),
+    "user_bytes": sorted(user_bytes.items(), key = lambda item:item[1], reverse=True),
+}
+
+with codecs.open(os.path.join(args.output, "report.html"), "w", encoding="utf-8") as fh:
+    fh.write(env.get_template("report.html").render(context))
+
+    # A more convenient way is to use env.get_template("...").render(locals())
+    # locals() is a dict which contains all locally defined variables ;)
+
+os.system("x-www-browser file://" + os.path.realpath("build/report.html") + " &")
 
 print "\n"
 print "Total lines:", total
@@ -157,17 +176,10 @@ else:
 print "\n"
 
 print("Top 5 visited users")
-results = users.items()
+results = user_bytes.items()
 results.sort(key = lambda item:item[1], reverse=True)
 for keyword, hits in results[:5]: #shows the first 5 results
     print keyword, "==>", hits, "(",hits * 100 / total, "%)"
-print "\n"
-
-print("Top 5 users bandwidth consumption")
-results = users.items()
-results.sort(key = lambda item:item[1], reverse=True)
-for user, transferred_bytes in results[:5]: #shows the first 5 results
-    print user, "==>", humanize(transferred_bytes)
 print "\n"
 
 print("Top IP addresses")
@@ -177,3 +189,6 @@ for source_ip, hits in results[:5]: #shows the first 5 results
     #print "Gotcha:", gi.country_code_by_addr("194.126.115.18").lower()
     print source_ip, "==>", hits, "(",hits * 100 / total, "%) " "==>", gi.country_code_by_addr(source_ip)
 print "\n"
+
+print "The value of __file__ is:", os.path.realpath(__file__)
+print "The directory of __file__ is:", os.path.realpath(os.path.dirname(__file__))
